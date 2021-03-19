d15e5e221221bed37b003f05396ff6cbf73b7f9d
Espiiii-debug <59062281+Espiiii-debug@users.noreply.github.com>
2021-03-19 22:33:53 +0100

    Fix GM declaration

--- src/me/espiiii/test/spigot/pluginTest/Main.java
+++ src/me/espiiii/test/spigot/pluginTest/Main.java
@@ -13,15 +13,17 @@ public class Main extends JavaPlugin
 	final static public String name = "TestPlugin";
 	final static public String author = "Espiiii";
 	final static public String version = "1.0.0 SNAPSHOT";
-	final static public String description = "Plugin utilisé pour des tests.";
+	final static public String description = "Plugin utilisÃ© pour des tests.";
 	
 	@Override
 	public void onEnable() 
 	{
 		
-		Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "Vous avez installé le plugin d'Espiiii. Il est nommé "+name);
+		Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "Vous avez installÃ© le plugin d'Espiiii. Il est nommÃ© "+name);
 		this.getCommand("kit").setExecutor(new CommandKit());
 		this.getCommand("welcome").setExecutor(new SayHelloCommand());
+		this.getCommand("gm").setExecutor(new CommandGM());
+
 		
 		super.onDisable();
 	}
@@ -29,7 +31,7 @@ public class Main extends JavaPlugin
 	public void onDisable() 
 	{
 		
-		Bukkit.getConsoleSender().sendMessage(ChatColor.BLUE + "Bonne journée, J'espère que le plugin " + name + " vous aura plu !");
+		Bukkit.getConsoleSender().sendMessage(ChatColor.BLUE + "Bonne journÃ©e, J'espÃ¨re que le plugin " + name + " vous aura plu !");
 		super.onDisable();
 		
 	}
